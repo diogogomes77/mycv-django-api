cbdd7953fb02b7328573491992ad4e1fe1e58357
Diogo Gomes <diogo.gomes77@gmail.com>
2022-04-23 18:13:32 +0100

    fix: heroku use settings heroku

--- mycv_django/asgi.py
+++ mycv_django/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycv_django.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycv_django.settings.bases')
 
 application = get_asgi_application()
