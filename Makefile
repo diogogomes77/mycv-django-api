c3e5e2668b518efd0fde364fd5ab69b944df7630
Diogo Gomes <diogo.gomes77@gmail.com>
2022-04-24 11:55:03 +0100

    feat(3-django-rest-framework): add create_data command

--- Makefile
+++ Makefile
@@ -19,16 +19,3 @@ test:
 
 create_data:
 	python manage.py create_data
-
-reset_db:
-	python manage.py reset_db --noinput --close-sessions
-	python manage.py migrate
-	python manage.py createsuperuser \
-		--username admin \
-		--email admin@example.com \
-		--noinput
-	python manage.py shell_plus --quiet-load -c "u = User.objects.get(username='admin');u.set_password('12345');u.save();"
-
-delete_migrations:
-	find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-	find . -path "*/migrations/*.pyc"  -delete
--- mycv_django/apps/common/management/commands/create_data.py
+++ mycv_django/apps/common/management/commands/create_data.py
@@ -9,10 +9,3 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         management.call_command('create_developers', number=5)
         management.call_command('create_managers', number=5)
-        management.call_command('create_countries', number=5)
-        management.call_command('create_businesses', number=5)
-        management.call_command('create_projects', number=5)
-        management.call_command('create_collaborations', number=25)
-        management.call_command('create_technologies', number=10)
-        management.call_command('create_project_technologies', number=10)
-        management.call_command('create_collaboration_technologies', number=10)
