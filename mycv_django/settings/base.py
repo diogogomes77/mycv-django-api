7899ef6f1d0f5a29992b66ac3b531d61c7eb68da
Diogo Gomes <diogo.gomes77@gmail.com>
2022-04-24 11:56:21 +0100

    feat(3-django-rest-framework): add serializers for developers managers users

--- mycv_django/settings/base.py
+++ mycv_django/settings/base.py
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
     # Third party apps
     'django_extensions',
     'rest_framework',
-    'drf_yasg',
 ] + INTERNAL_APPS
 
 
@@ -158,4 +157,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
