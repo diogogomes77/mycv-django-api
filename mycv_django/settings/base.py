7497464ff8abaec031902904047432151c9aee12
Diogo Gomes <diogo.gomes77@gmail.com>
2022-04-25 15:02:58 +0100

    fix: whitenoise staticfiles

--- mycv_django/settings/base.py
+++ mycv_django/settings/base.py
@@ -162,3 +162,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
