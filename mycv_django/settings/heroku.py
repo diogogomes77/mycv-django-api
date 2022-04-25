4d0863e0fb14894c4793ad80e8a9bd5fba0300e6
Diogo Gomes <diogo.gomes77@gmail.com>
2022-04-25 15:27:37 +0100

    fix: whitenoise staticfiles

--- mycv_django/settings/heroku.py
+++ mycv_django/settings/heroku.py
@@ -6,10 +6,3 @@ from .base import *  # noqa: F401,F403
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-# DATABASES = {
-#    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-# }
