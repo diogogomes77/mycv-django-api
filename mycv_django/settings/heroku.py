d517ffdb28523b15b9883f5138f5cbb1ff9dd7f4
Diogo Gomes <diogo.gomes77@gmail.com>
2022-04-25 19:47:44 +0100

    fix: whitenoise staticfiles

--- mycv_django/settings/heroku.py
+++ mycv_django/settings/heroku.py
@@ -6,3 +6,8 @@ from .base import *  # noqa: F401,F403
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+WHITENOISE_MANIFEST_STRICT = False
+STATICFILES_STORAGE = 'mycv_django.settings.storage.NoSourceMapsStorage'
+
+print('STATICFILES_STORAGE heroku: ', STATICFILES_STORAGE)
