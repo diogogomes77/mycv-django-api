e209d04ae8096c0e5b4fba50338e0c55fe858281
Diogo Gomes <diogo.gomes77@gmail.com>
2022-08-01 16:58:29 +0100

    feat(52): add slug field to Technologies

--- mycv_django/apps/technologies/models.py
+++ mycv_django/apps/technologies/models.py
@@ -15,6 +15,7 @@ class Technology(models.Model):
         related_name="children",
         symmetrical=False,
     )
+    slug = models.SlugField(null=False, unique=True)
 
     def __str__(self):
         return self.name
