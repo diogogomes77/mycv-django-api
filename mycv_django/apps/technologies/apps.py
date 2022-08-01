f49ccd4e6df36ea36c50dc15febb8f0bdc269a2c
Diogo Gomes <diogo.gomes77@gmail.com>
2022-08-01 16:59:31 +0100

    feat(52): add pre_signal slug creation to Technologies

--- mycv_django/apps/technologies/apps.py
+++ mycv_django/apps/technologies/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class TechnologiesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mycv_django.apps.technologies'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "mycv_django.apps.technologies"
+
+    def ready(self):
+        import mycv_django.apps.technologies.signals.handlers  # noqa: F401
