5a0e900deb1763e0a9a877aa1cfebb96bacb40e4
Diogo Gomes <diogo.gomes77@gmail.com>
2022-08-01 22:16:49 +0100

    feat(52): simplify project technologies api

--- mycv_django/apps/projects/serializers.py
+++ mycv_django/apps/projects/serializers.py
@@ -3,44 +3,37 @@ from rest_framework import serializers
 from mycv_django.apps.collaborations.serializers import CollaborationSerializer
 from mycv_django.apps.projects.models import Project
 from mycv_django.apps.technologies.models import ProjectTechnology
-from mycv_django.apps.technologies.serializers import TechnologySerializer
 
 
 class ProjectTechnologySerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectTechnology
-        fields = ("id", "comment", "name", "content", "technology")
-        depth = 1
+        fields = (
+            "technology_id",
+            "technology_slug",
+            "comment",
+            "technology_name",
+            "technology_content",
+            # "technology",
+        )
+        depth = 0
 
-    id = serializers.IntegerField(source="technology.id")
-    name = serializers.CharField(source="technology.name")
-    content = serializers.CharField(source="technology.content")
-    technology = TechnologySerializer()
+    technology_id = serializers.IntegerField(source="technology.id")
+    technology_slug = serializers.CharField(source="technology.slug")
+    technology_name = serializers.CharField(source="technology.name")
+    technology_content = serializers.CharField(source="technology.content")
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = "__all__"
-        depth = 1
+        depth = 0
         lookup_field = "slug"
 
     collaborations = CollaborationSerializer(
         source="collaboration_set", many=True, read_only=True
     )
-    technologies = ProjectTechnologySerializer(
-        source="projecttechnology_set", many=True, read_only=True
-    )
-
-
-class ProjectDetailSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Project
-        fields = "__all__"
-
-    collaborations = CollaborationSerializer(
-        source="collaboration_set", many=True, read_only=True
-    )
-    technologies = ProjectTechnologySerializer(
+    project_technologies = ProjectTechnologySerializer(
         source="projecttechnology_set", many=True, read_only=True
     )
--- mycv_django/apps/technologies/serializers.py
+++ mycv_django/apps/technologies/serializers.py
@@ -1,45 +1,44 @@
 from rest_framework import serializers
 
-from mycv_django.apps.collaborations.models import Collaboration
-from mycv_django.apps.projects.models import Project
+# from mycv_django.apps.collaborations.models import Collaboration
+# from mycv_django.apps.projects.models import Project
 from mycv_django.apps.technologies.models import Technology
 
-
-class TechnologyProjectSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Project
-        fields = ("id", "name", "slug")
-        lookup_field = "slug"
-
-
-class TechnologyCollaborationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Collaboration
-        fields = ("id", "collaborator", "project")
-        depth = 1
+# class TechnologyProjectSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Project
+#         fields = ("id", "name", "slug")
+#         lookup_field = "slug"
 
 
-class TechnologyParentCollaborationSerializer(TechnologyCollaborationSerializer):
-    class Meta(TechnologyCollaborationSerializer.Meta):
-        depth = 0
+# class TechnologyCollaborationSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Collaboration
+#         fields = ("id", "collaborator", "project")
+#         depth = 1
 
 
-class TechnologyParentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Technology
-        fields = "__all__"
+# class TechnologyParentCollaborationSerializer(TechnologyCollaborationSerializer):
+#     class Meta(TechnologyCollaborationSerializer.Meta):
+#         depth = 0
 
-    projects = TechnologyProjectSerializer(many=True)
-    collaborations = TechnologyParentCollaborationSerializer(many=True)
+
+# class TechnologyParentSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Technology
+#         fields = "__all__"
+
+#     projects = TechnologyProjectSerializer(many=True)
+#     collaborations = TechnologyParentCollaborationSerializer(many=True)
 
 
 class TechnologySerializer(serializers.ModelSerializer):
     class Meta:
         model = Technology
         fields = "__all__"
-        depth = 1
+        depth = 0
         lookup_field = "slug"
 
-    projects = TechnologyProjectSerializer(many=True)
-    collaborations = TechnologyCollaborationSerializer(many=True)
-    parents = TechnologyParentSerializer(many=True)
+    # projects = TechnologyProjectSerializer(many=True)
+    # collaborations = TechnologyCollaborationSerializer(many=True)
+    # parents = TechnologyParentSerializer(many=True)
