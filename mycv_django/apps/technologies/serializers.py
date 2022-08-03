2fce473c19d67841cec22b459f4244843492d934
Diogo Gomes <diogo.gomes77@gmail.com>
2022-08-03 19:57:30 +0100

    feat(52): technology parents slug list

--- mycv_django/apps/technologies/serializers.py
+++ mycv_django/apps/technologies/serializers.py
@@ -1,21 +1,22 @@
 from rest_framework import serializers
 
-# from mycv_django.apps.collaborations.models import Collaboration
-# from mycv_django.apps.projects.models import Project
+from mycv_django.apps.collaborations.models import Collaboration
+from mycv_django.apps.projects.models import Project
 from mycv_django.apps.technologies.models import Technology
 
-# class TechnologyProjectSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Project
-#         fields = ("id", "name", "slug")
-#         lookup_field = "slug"
+
+class TechnologyProjectSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+        fields = ("id", "name", "slug")
+        lookup_field = "slug"
 
 
-# class TechnologyCollaborationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Collaboration
-#         fields = ("id", "collaborator", "project")
-#         depth = 1
+class TechnologyCollaborationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collaboration
+        fields = ("id", "collaborator", "project")
+        depth = 0
 
 
 # class TechnologyParentCollaborationSerializer(TechnologyCollaborationSerializer):
@@ -23,13 +24,10 @@ from mycv_django.apps.technologies.models import Technology
 #         depth = 0
 
 
-# class TechnologyParentSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Technology
-#         fields = "__all__"
-
-#     projects = TechnologyProjectSerializer(many=True)
-#     collaborations = TechnologyParentCollaborationSerializer(many=True)
+class TechnologyParentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Technology
+        fields = ("slug",)
 
 
 class TechnologySerializer(serializers.ModelSerializer):
@@ -39,6 +37,10 @@ class TechnologySerializer(serializers.ModelSerializer):
         depth = 0
         lookup_field = "slug"
 
-    # projects = TechnologyProjectSerializer(many=True)
-    # collaborations = TechnologyCollaborationSerializer(many=True)
-    # parents = TechnologyParentSerializer(many=True)
+    # projects = serializers.ListField(source="projects_set")
+    # collaborations = serializers.IntegerField()
+    # parents = serializers.IntegerField()
+
+    projects = TechnologyProjectSerializer(many=True)
+    collaborations = TechnologyCollaborationSerializer(many=True)
+    parents = TechnologyParentSerializer(many=True)
